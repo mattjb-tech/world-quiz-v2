97902cbbe021ef09ff14478e994953a87b29dfa8
MatthewJBGithub <Matthewbone2@gmail.com>
2025-07-21 22:39:36 +0100

    Conditions Logic
    Started work on the props logic for the conditions component.

--- world-quiz/src/App.js
+++ world-quiz/src/App.js
@@ -5,18 +5,28 @@ import Conditions from './components/Conditions'
 import QuestionCard from './components/QuestionCard'
 import Results from './components/Results'
 
-// I've set it up so that the conditions component renders when the button is clicked.
-// We'll have to edit the {step === conditions ... } part later on when we're done with this component.
-// to add an onStart for the QuestionCards.
-// But for now, we can begin designing the Conditions component, and testing how it looks on screen.
+//Updated to include the Conditions component. For now, it doesn't have an 'onStart'
+// but we can add that later on when the component is closer to finished.
 
 function App() {
   const [step, setStep] = useState('welcome')
   const setConditions = () => setStep('conditions')
+
+// Because we're using React, our data has to start at the top level if we need multiple components to use it.
+// Then, we can send it to individual components as a prop later.
+
+  const [conditionList, setConditionList] = useState({  //the conditions the user picks for the quiz are an example of this
+    region: 'all',
+    question_count: 10
+  });
+
+
   return (
     <div className='app'>
       {step === 'welcome' && <WelcomeScreen onStart={setConditions} />} {/*if step = welcome, onstart lets us set the conditions, else it does nothing*/}
-      {step === 'conditions' && <Conditions />} {/*if step = conditions, render the conditions component instead.*/}
+      
+      {step === 'conditions' && <Conditions data={conditionList} 
+      updateData={setConditionList} />} 
     </div>
   );
 }
--- world-quiz/src/components/Conditions.js
+++ world-quiz/src/components/Conditions.js
@@ -1,5 +1,18 @@
-function Conditions(){
-    
+import React, { useState } from 'react';
+
+// Let me do this part <3 
+
+function Conditions({conditions, updateConditions}){
+    const handleChange = (e) => {
+        const {name , value } = e.target;
+
+        updateConditions({
+            ...conditions,
+            [name]: value
+        })
+    }
+
+
     return (
         <div className='conditions'>
 
